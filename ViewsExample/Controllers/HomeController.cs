1c19bdbab6884a6008612bf6a582fc5ef7fae9e3
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-26 15:59:05 +0100

    Views

---
+++ ViewsExample/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        public IActionResult Index()
+        {
+            // Views/Home/Index.cshtml
+            return View();
+            // abc.cshtml
+            // return View("abc");
+            // return new ViewResult() { ViewName = "abc" };
+        }
+    }
+}

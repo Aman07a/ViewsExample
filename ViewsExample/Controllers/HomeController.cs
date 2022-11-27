7bb41f25ed29bf06c9b0cee869f85a35af7a5e59
Aman Ahmed <aman16.aa17@gmail.com>
2022-11-27 15:07:50 +0100

    Strongly Typed Views - Part 2

--- ViewsExample/Controllers/HomeController.cs
+++ ViewsExample/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace ViewsExample.Controllers
         public IActionResult Index()
         {
             ViewData["appTitle"] = "Asp.Net Core Demo App";
+
             List<Person> people = new List<Person>()
             {
                 new Person()
@@ -35,5 +36,40 @@ namespace ViewsExample.Controllers
             // Views/Home/Index.cshtml
             return View("Index", people);
         }
+
+
+        [Route("person-details/{name}")]
+        public IActionResult Details(string? name)
+        {
+            if (name == null)
+                return Content("Person name can't be null");
+
+			List<Person> people = new List<Person>()
+			{
+				new Person()
+				{
+					Name = "John",
+					DateOfBirth = DateTime.Parse("2000-05-06"),
+					PersonGender = Gender.Male
+				},
+				new Person()
+				{
+					Name = "Linda",
+					DateOfBirth = DateTime.Parse("2006-01-09"),
+					PersonGender = Gender.Female
+				},
+				new Person()
+				{
+					Name = "Susan",
+					DateOfBirth = null,
+					PersonGender = Gender.Other
+				},
+			};
+
+			Person? matchingPerson = people.Where(temp => temp.Name == name).FirstOrDefault();
+
+            // Views/Home/Details.cshtml
+            return View(matchingPerson);
+		}
     }
 }
